3414c4b78550a01874eddf8d459a413f50432c56
Iván Pondal <ivan.pondal@gmail.com>
2024-02-16 12:30:23 -0300

    Add bundler analyzer

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+}
+
+module.exports = withBundleAnalyzer(nextConfig)

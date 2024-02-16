20b77044fbafdd8fdee71e746fb057cf74e2cc67
Iván Pondal <ivan.pondal@gmail.com>
2024-02-16 12:23:11 -0300

    Add margin auto

--- src/app/components/Waitlist.tsx
+++ src/app/components/Waitlist.tsx
@@ -3,12 +3,14 @@ import { Heading } from './Heading'
 import { Text } from './Text'
 import { TextField } from './TextField'
 import styles from './Waitlist.module.css'
+import utils from './css/utils.module.css'
 import Image from 'next/image'
 
 export const Waitlist = () => {
     return (
         <div className={styles.waitlist}>
             <Image
+                className={utils.marginAuto}
                 src={'star.svg'}
                 alt={'star drawing'}
                 width={212}

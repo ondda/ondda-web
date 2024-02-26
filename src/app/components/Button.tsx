614fc9d192db9a42e724363b2ccfbb1661ba85a2
Iván Pondal <ivan.pondal@gmail.com>
2024-02-26 16:15:54 -0300

    Add prettier tailwind config

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,6 +1,9 @@
 export const Button = () => {
     return (
-        <button className="text-4xl cursor-pointer transition-all px-4 py-6 rounded-xl font-sans font-semibold shadow-[3px_4px_1px_0px_rgba(0,0,0,0.25)]  border-[3px] border-gray-900 bg-pink-500 text-white">
+        <button className="cursor-pointer rounded-xl border-[3px] border-gray-900
+        bg-pink-500 px-4 py-6 font-sans text-4xl  font-semibold text-white
+        shadow-[3px_4px_1px_0px_rgba(0,0,0,0.25)] transition-all
+        hover:translate-x-[3px] hover:translate-y-[3px] hover:bg-[var(--primary-light-color)] hover:shadow-none">
             unirme
         </button>
     )

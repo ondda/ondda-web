3eeb482e0b4325170d027241ebfba66ce16f3e1c
Iván Pondal <ivan.pondal@gmail.com>
2024-02-26 15:45:57 -0300

    Add tailwind styled button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,7 @@
-import style from './Button.module.css'
-
 export const Button = () => {
-    return <button className={style.neoButton}>unirme</button>
+    return (
+        <button className="text-4xl cursor-pointer transition-all px-4 py-6 rounded-xl font-sans font-semibold shadow-[3px_4px_1px_0px_rgba(0,0,0,0.25)]  border-[3px] border-gray-900 bg-pink-500 text-white">
+            unirme
+        </button>
+    )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        fontFamily: {
+            sans: ['var(--font-public-sans)'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}

0074e795acd8c346d7041b8e1040bc86360657c2
Iván Pondal <ivan.pondal@gmail.com>
2024-02-26 16:24:22 -0300

    Fix styling

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,9 +1,11 @@
 export const Button = () => {
     return (
-        <button className="cursor-pointer rounded-xl border-[3px] border-gray-900
-        bg-pink-500 px-4 py-6 font-sans text-4xl  font-semibold text-white
+        <button
+            className="cursor-pointer rounded-xl border-[3px] border-gray-900
+        bg-[var(--primary-main-color)] px-4 py-6 font-sans text-4xl font-semibold text-white
         shadow-[3px_4px_1px_0px_rgba(0,0,0,0.25)] transition-all
-        hover:translate-x-[3px] hover:translate-y-[3px] hover:bg-[var(--primary-light-color)] hover:shadow-none">
+        hover:bg-[var(--primary-light-color)] active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
+        >
             unirme
         </button>
     )

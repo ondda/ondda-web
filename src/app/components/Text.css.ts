cab5789879934e8353d1488d56ba32f35de17683
Iván Pondal <ivan.pondal@gmail.com>
2024-03-01 17:27:06 -0300

    Add bold css rule

--- src/app/components/Text.css.ts
+++ src/app/components/Text.css.ts
@@ -1,9 +1,13 @@
 import { globalVars } from '../theme/globals.css'
-import { style } from '@vanilla-extract/css'
+import { globalStyle, style } from '@vanilla-extract/css'
 export const neoText = style({
     color: globalVars.color.gray,
     margin: 0,
     fontFamily: globalVars.font.sans.family,
     fontWeight: 200,
     textAlign: 'center',
+})
+
+globalStyle(`${neoText} b`, {
+    fontWeight: 500,
 })

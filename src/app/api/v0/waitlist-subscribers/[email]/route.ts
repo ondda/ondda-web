917d4fe33eb79643c399308c84ee511c5ca1e7cb
Iván Pondal <ivan.pondal@gmail.com>
2024-03-04 15:57:30 -0300

    Add insert query

--- src/app/api/v0/waitlist-subscribers/[email]/route.ts
+++ src/app/api/v0/waitlist-subscribers/[email]/route.ts
@@ -1,9 +1,27 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    connectionString:
+        'postgresql://root@127.0.0.1:26257/defaultdb?sslmode=disable',
+    max: 1,
+});
+
 export async function PUT(
     request: Request,
     { params }: { params: { email: string } }
 ) {
-    if (!isValidEmail(params.email)) {
+    const { email } = params;
+    if (!isValidEmail(email)) {
         return new Response('Invalid email', { status: 400 });
+    }
+
+    try {
+        await pool.query(
+            'INSERT INTO waitlist_subscriber (email) VALUES ($1)',
+            [email]
+        );
+    } catch (err) {
+        console.log(err);
     }
 
     return new Response(null, { status: 204 });

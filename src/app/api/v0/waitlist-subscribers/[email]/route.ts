e51811bf8c95093401635e619cfcc1b9520770b8
Iván Pondal <ivan.pondal@gmail.com>
2024-03-04 15:46:14 -0300

    Add email validation

--- src/app/api/v0/waitlist-subscribers/[email]/route.ts
+++ src/app/api/v0/waitlist-subscribers/[email]/route.ts
@@ -2,8 +2,16 @@ export async function PUT(
     request: Request,
     { params }: { params: { email: string } }
 ) {
-    console.log(request);
-    console.log(params);
+    if (!isValidEmail(params.email)) {
+        return new Response('Invalid email', { status: 400 });
+    }
 
     return new Response(null, { status: 204 });
 }
+
+const isValidEmail = (email: string): boolean => {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    // Check if the email matches the regular expression
+    return emailRegex.test(email.toLowerCase());
+};

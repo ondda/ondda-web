f768198c6c8860051e557d79fa32d0a2b328f529
Iván Pondal <ivan.pondal@gmail.com>
2024-03-06 17:16:14 -0300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
     title: 'ondda',
@@ -12,7 +13,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
